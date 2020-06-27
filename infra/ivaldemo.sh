d216ed8c3ef5e1f218806be646c5ef2c03f9cffb
Oliver Flatt <oflatt@warfa.cs.washington.edu>
2020-06-27 09:27:18 -0700

    don't generate empty files

--- infra/ivaldemo.sh
+++ infra/ivaldemo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 CORES=4
 
 report=$(git rev-parse --abbrev-ref HEAD)-$(date "+%Y-%m-%d")
@@ -12,10 +13,10 @@ function convertdemo {
     mkdir "bench/demo"
     racket infra/convert-demo.rkt "bench/demo" "infra/v10.json" "infra/v11.json" "infra/v12.json" "infra/v13.json"
 
-    racket infra/sort-fpbench-exprs.rkt "bench/demo/v10.fpcore" > "bench/demo/v10-s.fpcore"
-    racket infra/sort-fpbench-exprs.rkt "bench/demo/v11.fpcore" > "bench/demo/v11-s.fpcore"
-    racket infra/sort-fpbench-exprs.rkt "bench/demo/v12.fpcore" > "bench/demo/v12-s.fpcore"
-    racket infra/sort-fpbench-exprs.rkt "bench/demo/v13.fpcore" > "bench/demo/v13-s.fpcore"
+    racket infra/sort-fpbench-exprs.rkt "bench/demo/v10.fpcore" "bench/demo/v10-s.fpcore"
+    racket infra/sort-fpbench-exprs.rkt "bench/demo/v11.fpcore" "bench/demo/v11-s.fpcore"
+    racket infra/sort-fpbench-exprs.rkt "bench/demo/v12.fpcore" "bench/demo/v12-s.fpcore"
+    racket infra/sort-fpbench-exprs.rkt "bench/demo/v13.fpcore" "bench/demo/v13-s.fpcore"
     rm bench/demo/v10.fpcore bench/demo/v11.fpcore bench/demo/v12.fpcore bench/demo/v13.fpcore
 }
 
@@ -28,7 +29,7 @@ function convertfpbench {
 	name=$(basename "$bench" .fpcore)
 	echo "Converting FPBench file $name"
 	racket FPBench/transform.rkt --unroll 5 --skip-loops "$bench" "FPBench/converted/$name.fpcore"
-	racket infra/sort-fpbench-exprs.rkt "FPBench/converted/$name.fpcore" > "FPBench/converted/$name-s.fpcore"
+	racket infra/sort-fpbench-exprs.rkt "FPBench/converted/$name.fpcore" "FPBench/converted/$name-s.fpcore"
 	rm "FPBench/converted/$name.fpcore"
     done
 }
@@ -69,6 +70,7 @@ function runAll {
     done
     
     racket infra/nightly.rkt reports/ $dirs
+    echo "moving reports to $outputDir"
     rm -rf "$outputDir"
     mv reports "$outputDir"
 }
