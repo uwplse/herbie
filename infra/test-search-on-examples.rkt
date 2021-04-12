daed9c611e804be4e2d8b6aea94ec10dfa8226f4
oliver <oflatt@gmail.com>
2021-04-12 09:05:47 -0600

    check unsamplable as well

--- infra/test-search-on-examples.rkt
+++ infra/test-search-on-examples.rkt
@@ -4,6 +4,7 @@
 (define (perform-test lines)
   (define total 0)
   (define search-saved 0)
+  (define search-saved-unsamplable 0)
   (define groups (group-by (lambda (ele) (second ele)) lines))
   (for ([group groups])
        (define prog-unsugar (second (first group)))
@@ -16,12 +17,17 @@
        (println prog)
        (flush-output)
        (define sampler (make-sampler (get-representation 'binary64) `(lambda ,(second prog) TRUE)
-                                     (list prog) empty #t))
+
+                                                     (list prog) empty #t))
        (for ([line group])
-            (when (and (sampler (third line)) (fourth (fourth line))) ;; only domain errors
+            (define sampler-saved (sampler (third line)))
+            (when (and sampler-saved (fourth (fourth line))) ;; only domain errors
                   (set! search-saved (add1 search-saved)))
+            (when (and sampler-saved (sixth (fourth line)))
+                  (set! search-saved-unsamplable (add1 search-saved-unsamplable)))
+            
             (set! total (add1 total))
-            (println (list search-saved total)))))
+            (println (list total search-saved search-saved-unsamplable)))))
   
 
 (module+ main
